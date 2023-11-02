7f966f1e0b966ac568d251a9aea5d495ca59e92f
--global <stu2201261036@uni-plovdiv.bg>
2023-11-02 17:14:14 +0200

    3

--- mc/MyScript.js
+++ mc/MyScript.js
@@ -1,11 +1,14 @@
 const draggableElements = document.querySelectorAll('.draggable');
 
+let cardsArr = [];
+
 let isDragging = false;
 let currentElement = null;
 let x = 0;
 let y = 0;
 let z = 1;
-draggableElements.forEach((element) => {element.addEventListener('mousedown', startDrag);
+draggableElements.forEach((element) => {
+  element.addEventListener('mousedown', startDrag);
 });
 
 function startDrag(e) {
@@ -16,6 +19,16 @@ function startDrag(e) {
   z++;
   currentElement.style.zIndex = z;
   currentElement.style.cursor = 'grabbing';
+  const date = new Date();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const milliSeconds = String(e.timeStamp).split('.')[0].slice(-3).padStart(3, '0');
+  const time = `${hours}:${minutes}:${milliSeconds}`;
+  cardsArr.push({
+    id:currentElement.id,
+    date:time
+  });
+
   e.preventDefault();
   document.addEventListener('mousemove', dragElement);
   document.addEventListener('mouseup', endDrag);
